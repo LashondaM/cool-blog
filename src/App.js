71f79893c380b858e5fe83bd5e97e9c2c7c84f3f
Lashonda <lashondamiller007@gmail.com>
2022-01-27 15:29:40 -0500

    Dynamic Values in Templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const title = 'Welcome to the new blog';
+const likes = 50;
+// const person = {name: 'shonnie', age: 22}; can do bools and objects and try to output it like that
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+
+        {/* <p>{ person }</p> */}
+
+        <p>{ 10 }</p>
+        <p>{ "hello yall" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        
+      </div>
     </div>
   );
 }
