e5f6596fa52b726a66e2a23e67da6a8d98231626
Lashonda <lashondamiller007@gmail.com>
2022-02-01 10:59:01 -0500

    Adding Styles & Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    
+    const handleClick = (e) => {
+        console.log('hello yall', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+    
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('luigi', e)}>Click me again</button> {/* anonymous function */}
         </div>
      );
 }
