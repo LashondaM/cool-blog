9cbd05957f404900630642b7918274d50a643daa
Lashonda <lashondamiller007@gmail.com>
2022-02-01 11:59:22 -0500

    Using State (useState hook)

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,22 @@
-const Home = () => {
-    
-    const handleClick = (e) => {
-        console.log('hello yall', e);
-    }
+// state: the data being used in a component
+// hook: a special type of function that does a certain job, and starts with the word: use
+import { useState } from "react";
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
+const Home = () => {
+    // let name = 'luigi';
+    const [name, setName] = useState('luigi'); // name is the initial value: luigi, and setName is the function we can use to change the name value (video #8 time- 3:50)
+    const [age, setAge] = useState(25);
+    
+    const handleClick = () => {
+        setName('mario');
+        setAge(19);
     }
     
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('luigi', e)}>Click me again</button> {/* anonymous function */}
         </div>
      );
 }
