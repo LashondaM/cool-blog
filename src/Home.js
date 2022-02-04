d934417e5f4fc7bc5e2e31b0cd90991ec09e52fe
Lashonda <lashondamiller007@gmail.com>
2022-02-04 11:22:53 -0500

    Outputting Lists

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,20 @@
-// state: the data being used in a component
-// hook: a special type of function that does a certain job, and starts with the word: use
+// video #10
 import { useState } from "react";
 
 const Home = () => {
-    // let name = 'luigi';
-    const [name, setName] = useState('luigi'); // name is the initial value: luigi, and setName is the function we can use to change the name value (video #8 time- 3:50)
-    const [age, setAge] = useState(25);
-    
-    const handleClick = () => {
-        setName('mario');
-        setAge(19);
-    }
-    
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'luigi', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'peaches', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3 }
+    ]);
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => ( // blog is an item that we're calling each of the blogs as it goes through them
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))} {/* fires a callback function for each item and returns a bit of a JSX templet */}
         </div>
      );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,24 @@
 // type sfc and hit tab to make a stateless functional component
 const Navbar = () => {
+
+    function handleMouseOver(e) {
+        e.target.style.background = '#d3154e';
+    }
+
+    function handleMouseOff(e) {
+        e.target.style.background = '#f1356d';
+    }
+
     return ( 
         <nav className="navbar">
             <h1>The Cool Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create" style={{
+                <a href="/create" onMouseOver={handleMouseOver} onMouseLeave={handleMouseOff} style={{
                     color: "white",
                     backgroundColor: '#f1356d',
-                    borderRadius: '8px'
+                    borderRadius: '8px',
+                    transition: '0.3s'
                 }}>New Blog</a>
             </div>
         </nav>
